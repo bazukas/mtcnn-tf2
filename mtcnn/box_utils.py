d0255fd25c42753895bcaad097a2b92da9fd7d28
whoisltd <tiendat.le129@gmail.com>
2022-05-23 23:29:57 +0700

    update box_utils

--- mtcnn/box_utils.py
+++ mtcnn/box_utils.py
@@ -1,3 +1,4 @@
+from sklearn.preprocessing import scale
 import tensorflow as tf
 
 
@@ -11,6 +12,7 @@ def convert_to_square(bboxes):
         float tensor of shape [n, 4]
     """
     x1, y1, x2, y2 = [bboxes[:, i] for i in range(4)]
+    print(x1, y1, x2, y2)
     h = y2 - y1
     w = x2 - x1
     max_side = tf.maximum(h, w)
@@ -68,6 +70,7 @@ def get_image_boxes(bboxes, img, height, width, num_boxes, size=24):
                                          tf.zeros(num_boxes, dtype=tf.int32),
                                          (size, size))
     img_boxes = preprocess(img_boxes)
+    print(img_boxes.shape)
     return img_boxes
 
 
@@ -83,6 +86,7 @@ def generate_bboxes(probs, offsets, scale, threshold):
     Returns:
         float tensor of shape [n, 9]
     """
+
     # applying P-Net is equivalent, in some sense, to
     # moving 12x12 window with stride 2
     stride = 2
@@ -95,6 +99,7 @@ def generate_bboxes(probs, offsets, scale, threshold):
     if inds.shape[0] == 0:
         return tf.zeros((0, 9))
 
+    print('this is offsets:',offsets)
     # offsets: N x 4
     offsets = tf.gather_nd(offsets, inds)
     # score: N x 1
@@ -103,6 +108,7 @@ def generate_bboxes(probs, offsets, scale, threshold):
     # P-Net is applied to scaled images
     # so we need to rescale bounding boxes back
     inds = tf.cast(inds, tf.float32)
+    print('this is inds:',inds)
     # bounding_boxes: N x 9
     bounding_boxes = tf.concat([
         tf.expand_dims(tf.math.round((stride * inds[:, 1]) / scale), 1),
@@ -125,3 +131,47 @@ def preprocess(img):
     """
     img = (img - 127.5) * 0.0078125
     return img
+
+if __name__ == '__main__':
+    # print(convert_to_square(tf.constant([[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]])))
+
+    # print(calibrate_box(tf.constant([[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]]), 
+    # tf.constant([[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]])))
+
+    #test get_image_boxes
+    # boxes = tf.constant([[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]])
+    # img = tf.ones((100, 100, 3))
+    # height = 100
+    # width = 100
+    # num_boxes = 2
+    # size = 24
+    # get_image_boxes(boxes, img, height, width, num_boxes, size)
+
+    # test generate_bboxes
+    # probs = tf.constant([[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]])
+    # offsets = tf.constant([[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]])
+    # scale = 1.0
+    # threshold = 0.5
+    # generate_bboxes(probs, offsets, scale, threshold)
+
+    #probs shape [p, m, 2]
+    probs = tf.constant([[[0.1, 0.2], [0.5, 0.6], [0.7, 0.8]], [[0.1, 0.2], [0.5, 0.6], [0.7, 0.8]], [[0.1, 0.2], [0.5, 0.6], [0.7, 0.8]], [[0.1, 0.2], [0.5, 0.6], [0.7, 0.8]]])
+    # print(probs)
+    # probs = probs[:, :, 1]
+    #offsets shape [p, m, 4]
+    offsets = tf.constant([[[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]], [[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8]]])
+    # scale = 1.0
+    # threshold = 0.5
+    # generate_bboxes(probs, offsets, scale, threshold)
+    inds = tf.where(probs > 0.5)
+    print(inds)
+    print(inds[:,1])
+    print(inds[:,0])
+    print(inds[:,2])
+    # # inds = tf.zeros((0, 9))
+    # print(inds)
+    # print(inds[:, 1])
+    # tf.gather_nd(offsets, inds)
+
+
+    
